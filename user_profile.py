45d900a6963b33660432fdee3a4b14d0fb4a420d
johnff9 <157079795+johnff9@users.noreply.github.com>
2024-12-09 16:48:24 -0800

    UserProfile implementation complete

--- user_profile.py
+++ user_profile.py
@@ -2,38 +2,63 @@
 class UserProfile:
 
     # Constructor for a UserProfile object containing a variety of different fields that a UserProfile object will use
-    def __init__(self, name, location, relationship_status, age, occupation, astrological_sign, status=""):
-        self.name = name
-        self.location = location
-        self.relationship_status = relationship_status
-        self.age = age
-        self.occuptation = occupation
-        self.astrological_sign = astrological_sign
-        self.setStatus(status)
+    def __init__(self, name, location, relationship_status, age, occupation, astrological_sign, status="", friend_list=[]):
+        self.set_name(self, name)
+        self.set_location(self, location)
+        self.set_relationship_status(self, relationship_status)
+        self.set_age(self, age)
+        self.set_occupation(self, occupation)
+        self.set_astrological_sign(self, astrological_sign)
+        self.set_status(self, status)
+        self.friend_list = friend_list if friend_list is not None else []
 
     # Returns the name of the UserProfile
     def get_name(self):
         return self.name
+    
+    # Sets the name of the UserProfile
+    def set_name(self, name):
+        self.name = name
 
     # Return the location of the UserProfile
     def get_location(self):
         return self.location
+    
+    # Sets the location of the UserProfile
+    def set_location(self, location):
+        self.location = location
 
     # Return the relationship status of the UserProfile
     def get_relationship_status(self):
         return self.relationship_status
+    
+    # Sets the relationship_status of the UserProfile
+    def set_relationship_status(self, relationship_status):
+        self.relationship_status = relationship_status
 
     # Returns the age of the UserProfile
     def get_age(self):
         return self.age
+    
+    # Sets the age of the UserProfile
+    def set_age(self, age):
+        self.age = age
 
     # Returns the occupation of the UserProfile
     def get_occupation(self):
         return self.occuptation
+    
+    # Sets the occupation of the UserProfile
+    def set_occupation(self, occupation):
+        self.occupation = occupation
 
     # Returns the astrological sign of the UserProfile
     def get_astrological_sign(self):
         return self.astrological_sign
+    
+    # Sets the astrological_sign of the UserProfile
+    def set_astrological_sign(self, astrological_sign):
+        self.astrological_sign = astrological_sign
 
     # Returns the status of the 
     def get_status(self):
@@ -45,18 +70,24 @@ class UserProfile:
 
     # Returns the friends of this UserProfile
     def get_friends(self):
-        pass
+        return self.friend_list
 
     # Adds friend UserProfiles to this UserProfile's friends list
     def add_friend(self, friend_profile):
-        pass
+        self.friend_list.append(friend_profile)
 
     # Removes friend from UserProfile friends list
     def remove_friend(self, friend_profile):
-        pass
+        self.friend_list.remove(friend_profile)
 
     # Prints out the details of the UserProfile
     def print_details(self):
-        pass
-
+        print(f"Name: {self.get_name()}")
+        print(f"Location: {self.get_location()}")
+        print(f"Relationship Status: {self.get_relationship_status()}")
+        print(f"Age: {self.get_age()}")
+        print(f"Occupation: {self.get_occupation()}")
+        print(f"Astrological Sign: {self.get_astrological_sign()}")
+        print(f"Status: {self.get_status()}")
+        print(f"Friends: {[friend.get_name() for friend in self.get_friends()]}")
     
